baeafc1076297c0c179b0fc0158bd3fe40d3e81d
suwichada <jippee.1211@gmail.com>
2022-01-17 01:36:09 +0700

    lab16_1.cpp

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,24 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x; 
 
-	
+	cout<<a<<" "<<b<<" "<<c<<" "<<&x<<" "<<&y<<" "<<&z<<'\n';
+	cout<<&a<<" "<<&y<<" "<<&y<<" "<<&x<<" "<<&y<<" "<<&z<<'\n';
+	c = 'F';
+	cout<<a<<" "<<b<<" "<<c<<" "<<&x<<" "<<&y<<" "<<&z<<'\n';
+	*y ='W';
+	cout<<a<<" "<<b<<" "<<c<<" "<<&x<<" "<<&y<<" "<<&z<<'\n';
+	*x = 6;
+	cout<<a<<" "<<b<<" "<<c<<" "<<&x<<" "<<&y<<" "<<&z<<'\n';
+	**z = 7;
+	cout<<a<<" "<<b<<" "<<c<<" "<<&x<<" "<<&y<<" "<<&z<<'\n';
 	
 	return 0;
 }
