0642568b988b4a0295afe37bb0632e8fb119f9b2
suwichada <jippee.1211@gmail.com>
2022-01-17 01:37:13 +0700

    lab16_4.cpp

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,18 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &a,int &b,int &c,int &d){
+	int A,B,C,D;
+	int money[4]={1000,100,500,50};
+		do{
+		A = money[rand()%4];
+		B = money[rand()%4];
+		C = money[rand()%4];
+		D = money[rand()%4];
+		}while (A==B||A==C||A==D||B==C||B==D||C==D);
+		a = A;
+		b = B;
+		c = C;
+		d = D;
+		
+}
